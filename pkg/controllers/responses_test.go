c6e505fdd3d2727f5e369e5d1da647334d54816d
Brad Cypert <brad.cypert@gmail.com>
2020-05-24 13:43:41 -0400

    cleanup tests

--- pkg/controllers/responses_test.go
+++ pkg/controllers/responses_test.go
@@ -18,37 +18,37 @@ func TestResponses(t *testing.T) {
 	controller := GhastController{}
 
 	cases := []testcase{
-		testcase{
+		{
 			"200",
 			func(w http.ResponseWriter, r *http.Request) { controller.Success(w, "foo") },
 			200,
 			"",
 		},
-		testcase{
+		{
 			"403",
 			func(w http.ResponseWriter, r *http.Request) { controller.Forbidden(w, "foo") },
 			403,
 			"",
 		},
-		testcase{
+		{
 			"404",
 			func(w http.ResponseWriter, r *http.Request) { controller.NotFound(w, "foo") },
 			404,
 			"",
 		},
-		testcase{
+		{
 			"400",
 			func(w http.ResponseWriter, r *http.Request) { controller.BadRequest(w, "foo") },
 			400,
 			"",
 		},
-		testcase{
+		{
 			"401",
 			func(w http.ResponseWriter, r *http.Request) { controller.Unauthorized(w, "foo") },
 			401,
 			"",
 		},
-		testcase{
+		{
 			"500",
 			func(w http.ResponseWriter, r *http.Request) { controller.InternalServerError(w, "foo") },
 			500,
