7af1eafb9e61a190efac84d482f336615076e1ac
Brad Cypert <brad.cypert@gmail.com>
2020-11-05 20:34:22 -0500

    config tests

---
+++ pkg/config/config_parser_test.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"testing"
+)
+
+func TestConfigParser(t *testing.T) {
+	t.Run("Should parse a config properly", func(t *testing.T) {
+		k := map[string]interface{}{
+			"abc": map[string]interface{}{
+				"def": "ghi",
+			},
+			"r": 2138,
+		}
+
+		parsed, err := ParsedConfigToContainerKeys(k)
+
+		if err != nil {
+			t.Errorf("failed parsing config")
+		}
+
+		if parsed["r"] != 2138 {
+			t.Errorf("r not properly parsed")
+		}
+
+		if parsed["abc.def"] != "ghi" {
+			t.Errorf("expected: abc.def.ghi\ngot: %v", parsed["abc.def"])
+		}
+	})
+}
