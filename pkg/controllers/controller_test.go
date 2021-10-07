2b8662b2145de0c54b370651a7cb10ea745f91ac
Brad Cypert <brad.cypert@gmail.com>
2021-10-06 20:15:32 -0400

    feat: add unmarshall to controller

--- pkg/controllers/controller_test.go
+++ pkg/controllers/controller_test.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -48,5 +50,32 @@ func TestControllers(t *testing.T) {
 		if !test.Check(param) {
 			t.Errorf("%s - Query Param unexpected value: %s", test.Name, param)
 		}
+	}
+}
+
+func TestControllerUnmarshalling(t *testing.T) {
+
+	controller := GhastController{}
+	jsonBytes, _ := json.Marshal(struct {
+		Name string
+		Age  int
+	}{
+		"Brad",
+		28,
+	})
+
+	req := httptest.NewRequest(http.MethodPost, "/?name=name", bytes.NewReader(jsonBytes))
+
+	var user struct {
+		Name string
+		Age  int
+	}
+	controller.Unmarshall(req, &user)
+	if user.Name != "Brad" {
+		t.Errorf("Got: \"%s\"; expected value: \"Brad\"", user.Name)
+	}
+
+	if user.Age != 28 {
+		t.Errorf("Got: \"%d\"; expected value: 28", user.Age)
 	}
 }
