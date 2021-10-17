936404a9eeced5d3a06899f9f11fe5c903ce4f73
Brad Cypert <brad.cypert@gmail.com>
2021-10-16 22:52:12 -0400

    fix: 🐛 Cyclic dependency issue
    Move Resource to Router

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	ghastContainer "github.com/bradcypert/ghast/pkg/container"
-	ghastControllers "github.com/bradcypert/ghast/pkg/controllers"
 	pathToRegexp "github.com/soongo/path-to-regexp"
 )
 
@@ -21,6 +20,15 @@ const (
 	put     string = "PUT"
 	trace   string = "TRACE"
 )
+
+type Resource interface {
+	GetName() string
+	Index(w http.ResponseWriter, r *http.Request)
+	Get(w http.ResponseWriter, r *http.Request)
+	Create(w http.ResponseWriter, r *http.Request)
+	Update(w http.ResponseWriter, r *http.Request)
+	Delete(w http.ResponseWriter, r *http.Request)
+}
 
 // MiddlewareFunc is a functional alias to signify the signature of a middleware anonymous function.
 type MiddlewareFunc = func(*http.ResponseWriter, *http.Request)
@@ -103,110 +111,92 @@ func (r *Router) QueryParam(req *http.Request, key string) interface{} {
 }
 
 // Get registers a new GET route with the router
-func (r *Router) Get(route string, f func(http.ResponseWriter,
-	*http.Request)) *Router {
+func (r *Router) Get(route string, f http.HandlerFunc) *Router {
 	return r.route(get, route, f)
 }
 
 // GetM registers a new GET route with the router and wires up the given middleware for that route only
-func (r *Router) GetM(route string, f func(http.ResponseWriter,
-	*http.Request), middleware []MiddlewareFunc) *Router {
+func (r *Router) GetM(route string, f http.HandlerFunc, middleware []MiddlewareFunc) *Router {
 	return r.routeM(get, route, f, middleware)
 }
 
 // Post registers a new POST route with the router
-func (r *Router) Post(route string, f func(http.ResponseWriter,
-	*http.Request)) *Router {
+func (r *Router) Post(route string, f http.HandlerFunc) *Router {
 	return r.route(post, route, f)
 }
 
 // PostM registers a new POST route with the router and wires up the given middleware for that route only
-func (r *Router) PostM(route string, f func(http.ResponseWriter,
-	*http.Request), middleware []MiddlewareFunc) *Router {
+func (r *Router) PostM(route string, f http.HandlerFunc, middleware []MiddlewareFunc) *Router {
 	return r.routeM(post, route, f, middleware)
 }
 
 // Put registers a new PUT route with the router
-func (r *Router) Put(route string, f func(http.ResponseWriter,
-	*http.Request)) *Router {
+func (r *Router) Put(route string, f http.HandlerFunc) *Router {
 	return r.route(put, route, f)
 }
 
 // PutM registers a new PUT route with the router and wires up the given middleware for that route only
-func (r *Router) PutM(route string, f func(http.ResponseWriter,
-	*http.Request), middleware []MiddlewareFunc) *Router {
+func (r *Router) PutM(route string, f http.HandlerFunc, middleware []MiddlewareFunc) *Router {
 	return r.routeM(put, route, f, middleware)
 }
 
 // Patch registers a new PATCH route with the router
-func (r *Router) Patch(route string, f func(http.ResponseWriter,
-	*http.Request)) *Router {
+func (r *Router) Patch(route string, f http.HandlerFunc) *Router {
 	return r.route(patch, route, f)
 }
 
 // PatchM registers a new PATCH route with the router and wires up the given middleware for that route only
-func (r *Router) PatchM(route string, f func(http.ResponseWriter,
-	*http.Request), middleware []MiddlewareFunc) *Router {
+func (r *Router) PatchM(route string, f http.HandlerFunc, middleware []MiddlewareFunc) *Router {
 	return r.routeM(patch, route, f, middleware)
 }
 
 // Delete registers a new DELETE route with the router
-func (r *Router) Delete(route string, f func(http.ResponseWriter,
-	*http.Request)) *Router {
+func (r *Router) Delete(route string, f http.HandlerFunc) *Router {
 	return r.route(delete, route, f)
 }
 
 // DeleteM registers a new DELETE route with the router and wires up the given middleware for that route only
-func (r *Router) DeleteM(route string, f func(http.ResponseWriter,
-	*http.Request), middleware []MiddlewareFunc) *Router {
+func (r *Router) DeleteM(route string, f http.HandlerFunc, middleware []MiddlewareFunc) *Router {
 	return r.routeM(delete, route, f, middleware)
 }
 
 // Options registers a new OPTIONS route with the router
-func (r *Router) Options(route string, f func(http.ResponseWriter,
-	*http.Request)) *Router {
+func (r *Router) Options(route string, f http.HandlerFunc) *Router {
 	return r.route(options, route, f)
 }
 
 // OptionsM registers a new OPTIONS route with the router and wires up the given middleware for that route only
-func (r *Router) OptionsM(route string, f func(http.ResponseWriter,
-	*http.Request), middleware []MiddlewareFunc) *Router {
+func (r *Router) OptionsM(route string, f http.HandlerFunc, middleware []MiddlewareFunc) *Router {
 	return r.routeM(options, route, f, middleware)
 }
 
 // Head registers a new HEAD route with the router
-func (r *Router) Head(route string, f func(http.ResponseWriter,
-	*http.Request)) *Router {
+func (r *Router) Head(route string, f http.HandlerFunc) *Router {
 	return r.route(head, route, f)
 }
 
 // HeadM registers a new HEAD route with the router and wires up the given middleware for that route only
-func (r *Router) HeadM(route string, f func(http.ResponseWriter,
-	*http.Request), middleware []MiddlewareFunc) *Router {
+func (r *Router) HeadM(route string, f http.HandlerFunc, middleware []MiddlewareFunc) *Router {
 	return r.routeM(head, route, f, middleware)
 }
 
 // Trace registers a new TRACE route with the router
-func (r *Router) Trace(route string, f func(http.ResponseWriter,
-	*http.Request)) *Router {
+func (r *Router) Trace(route string, f http.HandlerFunc) *Router {
 	return r.route(trace, route, f)
 }
 
 // TraceM registers a new TRACE route with the router and wires up the given middleware for that route only
-func (r *Router) TraceM(route string, f func(http.ResponseWriter,
-	*http.Request), middleware []MiddlewareFunc) *Router {
+func (r *Router) TraceM(route string, f http.HandlerFunc, middleware []MiddlewareFunc) *Router {
 	return r.routeM(trace, route, f, middleware)
 }
 
 // Connect registers a new CONNECT route with the router
-func (r *Router) Connect(route string, f func(http.ResponseWriter,
-	*http.Request)) *Router {
+func (r *Router) Connect(route string, f http.HandlerFunc) *Router {
 	return r.route(connect, route, f)
 }
 
 // ConnectM registers a new CONNECT route with the router and wires up the given middleware for that route only
-func (r *Router) ConnectM(route string, f func(http.ResponseWriter,
-	*http.Request), middleware []MiddlewareFunc) *Router {
+func (r *Router) ConnectM(route string, f http.HandlerFunc, middleware []MiddlewareFunc) *Router {
 	return r.routeM(connect, route, f, middleware)
 }
 
@@ -220,7 +210,7 @@ func (r *Router) ConnectM(route string, f func(http.ResponseWriter,
 // POST    /v1/user
 // DELETE  /v1/user/:id
 // UPDATE /v1/user/:id
-func (r *Router) Resource(prefix string, resource ghastControllers.Resource) {
+func (r *Router) Resource(prefix string, resource Resource) {
 	r.Get(prefix+resource.GetName(), resource.Index)
 	r.Get(prefix+resource.GetName()+"/:id", resource.Get)
 	r.Post(prefix+resource.GetName(), resource.Create)
@@ -241,14 +231,12 @@ func (r Router) DefaultServer() *http.Server {
 }
 
 // Format the route binding
-func (r *Router) route(method string, route string, f func(http.ResponseWriter,
-	*http.Request)) *Router {
+func (r *Router) route(method string, route string, f http.HandlerFunc) *Router {
 	r.binding = append(r.binding, Binding{match: route, handler: f, method: method})
 	return r
 }
 
-func (r *Router) routeM(method string, route string, f func(http.ResponseWriter,
-	*http.Request), middleware []MiddlewareFunc) *Router {
+func (r *Router) routeM(method string, route string, f http.HandlerFunc, middleware []MiddlewareFunc) *Router {
 	r.binding = append(r.binding, Binding{match: route, handler: f, middlewares: middleware, method: method})
 	return r
 }
