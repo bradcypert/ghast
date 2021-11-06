cae08a508fbd5a557e1edc87e8482ba4735186a1
Brad Cypert <brad.cypert@gmail.com>
2021-11-06 12:54:54 -0400

    fix: 🐛 Modify request logger to work with new middleware

--- pkg/middleware/request_logger.go
+++ pkg/middleware/request_logger.go
@@ -8,10 +8,18 @@ import (
 
 // RequestLogger middleware
 // Logs incoming requests by URL and body
-func RequestLogger(rw *http.ResponseWriter, req *http.Request) {
-	fmt.Println("Incoming Request: " + req.URL.String())
-	body, err := ioutil.ReadAll(req.Body)
-	if err == nil && body != nil {
-		fmt.Println(body)
-	}
+func RequestLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		fmt.Println("Incoming Request: " + req.URL.String())
+		body, err := ioutil.ReadAll(req.Body)
+		if err == nil && body != nil {
+			fmt.Println(body)
+		}
+
+		if err != nil {
+			fmt.Println("[Error]: Error when reading body on incoming request,", err)
+		}
+
+		next.ServeHTTP(rw, req)
+	})
 }
